5677f0ca320b9932105519e69cf4eca8cab8dc4a
Alan <alan.cueva@yahoo.es>
2023-05-08 12:33:30 -0500

    Agregadas variables de entorno en .env

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -1,23 +1,3 @@
-// const { initializeApp } = require('firebase/app');
-// const { getFirestore, collection, getDocs, doc, setDoc, query, where, updateDoc, deleteDoc, getDoc, addDoc } = require('firebase/firestore');
-// const { getAuth, createUserWithEmailAndPassword, updateProfile} = require("firebase/auth");
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBCSMzzP5UD-7g_Mi1FEOtfj2IUjZoQaYg",
-//   authDomain: "ingweb-ap.firebaseapp.com",
-//   projectId: "ingweb-ap",
-//   storageBucket: "ingweb-ap.appspot.com",
-//   messagingSenderId: "936544478001",
-//   appId: "1:936544478001:web:563092b879350a2d878610",
-//   measurementId: "G-WV9F4WD9QM"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-// const auth = getAuth();
-
-// module.exports = { auth, collection, db, getDocs, createUserWithEmailAndPassword, doc, setDoc, updateProfile, query, where, updateDoc, deleteDoc, getDoc, addDoc};
-
 
 const admin = require('firebase-admin');
 
@@ -29,7 +9,7 @@ const credenciales = require(rutaArchivoCredenciales);
 
 admin.initializeApp({
   credential: admin.credential.cert(credenciales),
-  databaseURL: 'firebase-adminsdk-yaj28@ingweb-ap.iam.gserviceaccount.com' // Reemplaza <TU_PROYECTO_FIREBASE> con el nombre de tu proyecto en Firebase
+  databaseURL: `${process.env.ACCOUNT_SERVICE_URL}`
 });
 
 // Ahora puedes usar el paquete firebase-admin para interactuar con Firebase en tu aplicación
